f858e205d6ed552dd4f9b96a167f520248a0ffd4
Roland Kersche <roland.kersche@bitmovin.com>
2016-11-04 13:59:10 +0100

    Updated naming of variable configuration

--- examples/CreateContentProtectedDashTest.php
+++ examples/CreateContentProtectedDashTest.php
@@ -19,16 +19,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $client = new BitmovinClient('INSERT YOUR API KEY HERE');
 
 // CONFIGURATION
-$config = array();
-$config['videoInputPath'] = 'INSERT YOUR HTTP VIDEO INPUT PATH HERE';
-$config['accessKey'] = 'INSERT YOUR GCS OUTPUT ACCESS KEY HERE';
-$config['secretKey'] = 'INSERT YOUR GCS OUTPUT SECRET KEY HERE';
-$config['bucketName'] = 'INSERT YOUR GCS OUTPUT BUCKET NAME HERE';
-$config['prefix'] = 'INSERT YOUR GCS OUTPUT PREFIX (FOLDER) HERE';
-$config['key'] = 'INSERT YOUR CENC DRM KEY HERE';
-$config['kid'] = 'INSERT YOUR CENC DRM KID HERE';
-$config['widevine_pssh'] = 'INSERT YOUR CENC WIDEVINE PSSH HERE';
-$config['playready_laurl'] = 'INSERT YOUR CENC PLAYREADY LAURL HERE';
+$videoInputPath = 'INSERT YOUR HTTP VIDEO INPUT PATH HERE';
+$gcs_accessKey = 'INSERT YOUR GCS OUTPUT ACCESS KEY HERE';
+$gcs_secretKey = 'INSERT YOUR GCS OUTPUT SECRET KEY HERE';
+$gcs_bucketName = 'INSERT YOUR GCS OUTPUT BUCKET NAME HERE';
+$gcs_prefix = 'INSERT YOUR GCS OUTPUT PREFIX (FOLDER) HERE';
+$cenc_drm_key = 'INSERT YOUR CENC DRM KEY HERE';
+$cenc_drm_kid= 'INSERT YOUR CENC DRM KID HERE';
+$cenc_widevine_pssh = 'INSERT YOUR CENC WIDEVINE PSSH HERE';
+$cenc_playready_laurl = 'INSERT YOUR CENC PLAYREADY LAURL HERE';
 
 // CREATE ENCODING PROFILE
 $encodingProfile = new EncodingProfileConfig();
@@ -37,7 +36,7 @@ $encodingProfile->cloudRegion = CloudRegion::GOOGLE_EUROPE_WEST_1;
 
 // CREATE VIDEO STREAM CONFIGS
 $videoStreamConfig_1080 = new H264VideoStreamConfig();
-$videoStreamConfig_1080->input = new HttpInput($config['videoInputPath']);
+$videoStreamConfig_1080->input = new HttpInput($videoInputPath);
 $videoStreamConfig_1080->width = 1920;
 $videoStreamConfig_1080->height = 1080;
 $videoStreamConfig_1080->bitrate = 4800000;
@@ -45,7 +44,7 @@ $videoStreamConfig_1080->rate = 25.0;
 $encodingProfile->videoStreamConfigs[] = $videoStreamConfig_1080;
 
 $videoStreamConfig_720 = new H264VideoStreamConfig();
-$videoStreamConfig_720->input = new HttpInput($config['videoInputPath']);
+$videoStreamConfig_720->input = new HttpInput($videoInputPath);
 $videoStreamConfig_720->width = 1280;
 $videoStreamConfig_720->height = 720;
 $videoStreamConfig_720->bitrate = 2400000;
@@ -53,7 +52,7 @@ $videoStreamConfig_720->rate = 25.0;
 $encodingProfile->videoStreamConfigs[] = $videoStreamConfig_720;
 
 $videoStreamConfig_480 = new H264VideoStreamConfig();
-$videoStreamConfig_480->input = new HttpInput($config['videoInputPath']);
+$videoStreamConfig_480->input = new HttpInput($videoInputPath);
 $videoStreamConfig_480->width = 854;
 $videoStreamConfig_480->height = 480;
 $videoStreamConfig_480->bitrate = 1200000;
@@ -61,7 +60,7 @@ $videoStreamConfig_480->rate = 25.0;
 $encodingProfile->videoStreamConfigs[] = $videoStreamConfig_480;
 
 $videoStreamConfig_360 = new H264VideoStreamConfig();
-$videoStreamConfig_360->input = new HttpInput($config['videoInputPath']);
+$videoStreamConfig_360->input = new HttpInput($videoInputPath);
 $videoStreamConfig_360->width = 640;
 $videoStreamConfig_360->height = 360;
 $videoStreamConfig_360->bitrate = 800000;
@@ -69,7 +68,7 @@ $videoStreamConfig_360->rate = 25.0;
 $encodingProfile->videoStreamConfigs[] = $videoStreamConfig_360;
 
 $videoStreamConfig_240 = new H264VideoStreamConfig();
-$videoStreamConfig_240->input = new HttpInput($config['videoInputPath']);
+$videoStreamConfig_240->input = new HttpInput($videoInputPath);
 $videoStreamConfig_240->width = 426;
 $videoStreamConfig_240->height = 240;
 $videoStreamConfig_240->bitrate = 400000;
@@ -78,7 +77,7 @@ $encodingProfile->videoStreamConfigs[] = $videoStreamConfig_240;
 
 // CREATE AUDIO STREAM CONFIG
 $audioConfig = new AudioStreamConfig();
-$audioConfig->input = new HttpInput($config['videoInputPath']);
+$audioConfig->input = new HttpInput($videoInputPath);
 $audioConfig->bitrate = 128000;
 $audioConfig->rate = 48000;
 $audioConfig->name = 'English';
@@ -89,14 +88,14 @@ $encodingProfile->audioStreamConfigs[] = $audioConfig;
 // CREATE JOB CONFIG
 $jobConfig = new JobConfig();
 // ASSIGN OUTPUT
-$jobConfig->output = new GcsOutput($config['accessKey'], $config['secretKey'], $config['bucketName'], $config['prefix']);
+$jobConfig->output = new GcsOutput($gcs_accessKey, $gcs_secretKey, $gcs_bucketName, $gcs_prefix);
 // ASSIGN ENCODING PROFILES TO JOB
 $jobConfig->encodingProfile = $encodingProfile;
 // ENABLE DASH OUTPUT WITH CENC
 $outputFormat = new DashOutputFormat();
-$outputFormat->cenc = new CencDrm($config['key'], $config['kid']);
-$outputFormat->cenc->setWidevine(new CencWidevine($config['widevine_pssh']));
-$outputFormat->cenc->setPlayReady(new CencPlayReady($config['playready_laurl']));
+$outputFormat->cenc = new CencDrm($cenc_drm_key, $cenc_drm_kid);
+$outputFormat->cenc->setWidevine(new CencWidevine($cenc_widevine_pssh));
+$outputFormat->cenc->setPlayReady(new CencPlayReady($cenc_playready_laurl));
 $outputFormat->cenc->setMarlin(new CencMarlin());
 $jobConfig->outputFormat[] = $outputFormat;
 
--- examples/CreateSimpleEncodingAsync.php
+++ examples/CreateSimpleEncodingAsync.php
@@ -17,12 +17,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $client = new BitmovinClient('INSERT YOUR API KEY HERE');
 
 // CONFIGURATION
-$config = array();
-$config['videoInputPath'] = 'http://eu-storage.bitcodin.com/inputs/Sintel.2010.720p.mkv';
-$config['accessKey'] = 'YOUR GCS ACCESS KEY';
-$config['secretKey'] = 'YOUR GCS SECRET KEY';
-$config['bucketName'] = 'YOUR GCS BUCKET NAME';
-$config['prefix'] = 'path/to/your/output/destination/';
+$videoInputPath = 'http://eu-storage.bitcodin.com/inputs/Sintel.2010.720p.mkv';
+$gcs_accessKey = 'INSERT YOUR GCS OUTPUT ACCESS KEY HERE';
+$gcs_secretKey = 'INSERT YOUR GCS OUTPUT SECRET KEY HERE';
+$gcs_bucketName = 'INSERT YOUR GCS OUTPUT BUCKET NAME HERE';
+$gcs_prefix = 'path/to/your/output/destination/';
 
 // CREATE ENCODING PROFILE
 $encodingProfile = new EncodingProfileConfig();
@@ -31,7 +30,7 @@ $encodingProfile->cloudRegion = CloudRegion::GOOGLE_EUROPE_WEST_1;
 
 // CREATE VIDEO STREAM CONFIG FOR 1080p
 $videoStreamConfig_1080 = new H264VideoStreamConfig();
-$videoStreamConfig_1080->input = new HttpInput($config['videoInputPath']);
+$videoStreamConfig_1080->input = new HttpInput($videoInputPath);
 $videoStreamConfig_1080->width = 1920;
 $videoStreamConfig_1080->height = 1080;
 $videoStreamConfig_1080->bitrate = 4800000;
@@ -40,7 +39,7 @@ $encodingProfile->videoStreamConfigs[] = $videoStreamConfig_1080;
 
 // CREATE VIDEO STREAM CONFIG FOR 720p
 $videoStreamConfig_720 = new H264VideoStreamConfig();
-$videoStreamConfig_720->input = new HttpInput($config['videoInputPath']);
+$videoStreamConfig_720->input = new HttpInput($videoInputPath);
 $videoStreamConfig_720->width = 1280;
 $videoStreamConfig_720->height = 720;
 $videoStreamConfig_720->bitrate = 2400000;
@@ -49,7 +48,7 @@ $encodingProfile->videoStreamConfigs[] = $videoStreamConfig_720;
 
 // CREATE AUDIO STREAM CONFIG
 $audioConfig = new AudioStreamConfig();
-$audioConfig->input = new HttpInput($config['videoInputPath']);
+$audioConfig->input = new HttpInput($videoInputPath);
 $audioConfig->bitrate = 128000;
 $audioConfig->rate = 48000;
 $audioConfig->name = 'English';
@@ -60,7 +59,7 @@ $encodingProfile->audioStreamConfigs[] = $audioConfig;
 // CREATE JOB CONFIG
 $jobConfig = new JobConfig();
 // ASSIGN OUTPUT
-$jobConfig->output = new GcsOutput($config['accessKey'], $config['secretKey'], $config['bucketName'], $config['prefix']);
+$jobConfig->output = new GcsOutput($gcs_accessKey, $gcs_secretKey, $gcs_bucketName, $gcs_prefix);
 // ASSIGN ENCODING PROFILES TO JOB
 $jobConfig->encodingProfile = $encodingProfile;
 // ENABLE DASH OUTPUT
