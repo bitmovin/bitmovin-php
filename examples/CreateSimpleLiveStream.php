157302041cb4ea0c9da766238ed7691ab7fa7c06
AndreasRudich <andreas.rudich@bitmovin.com>
2016-11-04 11:34:12 +0100

    Fixed high-level live stream example.

--- examples/CreateSimpleLiveStream.php
+++ examples/CreateSimpleLiveStream.php
@@ -8,20 +8,20 @@ use Bitmovin\configs\LiveStreamJobConfig;
 use Bitmovin\configs\manifest\DashOutputFormat;
 use Bitmovin\configs\manifest\HlsOutputFormat;
 use Bitmovin\configs\video\H264VideoStreamConfig;
-use Bitmovin\input\HttpInput;
+use Bitmovin\input\RtmpInput;
 use Bitmovin\output\GcsOutput;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$client = new BitmovinClient('INSERT YOUR API KEY HERE');
+$client = new BitmovinClient('YOUR API KEY');
 
 // CONFIGURATION
 $config = array();
-$config['videoInputPath'] = 'http://eu-storage.bitcodin.com/inputs/Sintel.2010.720p.mkv';
-$config['accessKey'] = 'YOUR GCS ACCESS KEY';
-$config['secretKey'] = 'YOUR GCS SECRET KEY';
+$config['accessKey']  = 'YOUR GCS ACCESS KEY';
+$config['secretKey']  = 'YOUR GCS SECRET KEY';
 $config['bucketName'] = 'YOUR GCS BUCKET NAME';
-$config['prefix'] = 'path/to/your/output/destination/';
+$config['prefix']     = 'path/to/your/output/destination/';
+$config['streamKey']  = 'YOUR STREAM KEY';
 
 // CREATE ENCODING PROFILE
 $encodingProfile = new EncodingProfileConfig();
@@ -30,7 +30,7 @@ $encodingProfile->cloudRegion = CloudRegion::GOOGLE_EUROPE_WEST_1;
 
 // CREATE VIDEO STREAM CONFIG FOR 1080p
 $videoStreamConfig_1080 = new H264VideoStreamConfig();
-$videoStreamConfig_1080->input = new HttpInput($config['videoInputPath']);
+$videoStreamConfig_1080->input = new RtmpInput();
 $videoStreamConfig_1080->width = 1920;
 $videoStreamConfig_1080->height = 1080;
 $videoStreamConfig_1080->bitrate = 4800000;
@@ -39,7 +39,7 @@ $encodingProfile->videoStreamConfigs[] = $videoStreamConfig_1080;
 
 // CREATE VIDEO STREAM CONFIG FOR 720p
 $videoStreamConfig_720 = new H264VideoStreamConfig();
-$videoStreamConfig_720->input = new HttpInput($config['videoInputPath']);
+$videoStreamConfig_720->input = new RtmpInput();
 $videoStreamConfig_720->width = 1280;
 $videoStreamConfig_720->height = 720;
 $videoStreamConfig_720->bitrate = 2400000;
@@ -48,7 +48,7 @@ $encodingProfile->videoStreamConfigs[] = $videoStreamConfig_720;
 
 // CREATE AUDIO STREAM CONFIG
 $audioConfig = new AudioStreamConfig();
-$audioConfig->input = new HttpInput($config['videoInputPath']);
+$audioConfig->input = new RtmpInput();
 $audioConfig->bitrate = 128000;
 $audioConfig->rate = 48000;
 $audioConfig->name = 'English';
@@ -59,7 +59,7 @@ $encodingProfile->audioStreamConfigs[] = $audioConfig;
 // CREATE JOB CONFIG
 $jobConfig = new LiveStreamJobConfig();
 // ASSIGN STREAM KEY
-$jobConfig->streamKey = 'YOUR STREAM KEY';
+$jobConfig->streamKey = $config['streamKey'];
 // ASSIGN OUTPUT
 $jobConfig->output = new GcsOutput($config['accessKey'], $config['secretKey'], $config['bucketName'], $config['prefix']);
 // ASSIGN ENCODING PROFILES TO JOB
@@ -69,5 +69,12 @@ $jobConfig->outputFormat[] = new DashOutputFormat();
 // ENABLE HLS OUTPUT
 $jobConfig->outputFormat[] = new HlsOutputFormat();
 
-// RUN JOB AND WAIT UNTIL IT HAS FINISHED
-$client->runJobAndWaitForCompletion($jobConfig);
+// START LIVE STREAM
+$jobContainer = $client->startJob($jobConfig);
+
+// WAIT UNTIL LIVE STREAM IS RUNNING
+$client->waitForJobsToStart($jobContainer);
+
+// TODO: RETRIEVE LIVE STREAM DATA WHEN AVAILABLE
+
+// TODO: STOP LIVE STREAM
