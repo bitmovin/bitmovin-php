5706da7ad53c89bcefe11960d3dc29622759e5b4
Gernot Zwantschko <raeumungsdienst@gmx-topmail.de>
2016-12-02 14:51:43 +0100

    ThumbnailConfig.php: Created for high lvl client

---
+++ src/Bitmovin/configs/images/ThumbnailConfig.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Bitmovin\configs;
+
+
+use Bitmovin\configs\audio\AudioStreamConfig;
+use Bitmovin\configs\video\AbstractVideoStreamConfig;
+
+class ThumbnailConfig
+{
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var  string
+     */
+    public $description;
+
+    /**
+     * @var  integer
+     */
+    public $height;
+
+    /**
+     * @var  array
+     */
+    public $positions = array();
+
+    /**
+     * @var  string
+     */
+    public $pattern = "thumb_%number%.png";
+
+    /**
+     * ThumbnailConfig constructor.
+     * @param       $height
+     * @param array $positions
+     */
+    public function __construct($height, array $positions)
+    {
+        $this->height = $height;
+        $this->positions = $positions;
+    }
+
+
+}
