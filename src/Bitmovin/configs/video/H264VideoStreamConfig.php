acd1e60780018646b8aa9c7676af72bd8f924c2d
Gernot Zwantschko <raeumungsdienst@gmx-topmail.de>
2016-12-02 14:52:16 +0100

    H264VideoStreamConfig.php: Add ThumbnailConfigs

--- src/Bitmovin/configs/video/H264VideoStreamConfig.php
+++ src/Bitmovin/configs/video/H264VideoStreamConfig.php
@@ -5,6 +5,7 @@ namespace Bitmovin\configs\video;
 
 
 use Bitmovin\api\enum\codecConfigurations\H264Profile;
+use Bitmovin\configs\ThumbnailConfig;
 
 class H264VideoStreamConfig extends AbstractVideoStreamConfig
 {
@@ -12,4 +13,7 @@ class H264VideoStreamConfig extends AbstractVideoStreamConfig
      * @var string Enum: \Bitmovin\api\enum\codecConfigurations\H264Profile
      */
     public $profile = H264Profile::HIGH;
+
+    /** @var ThumbnailConfig[] */
+    public $thumbnails = array();
 }
