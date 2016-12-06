4c98d34ccb4bf4dfc8a52eddbdeae07e884d61c3
Gernot Zwantschko <raeumungsdienst@gmx-topmail.de>
2016-12-06 11:47:17 +0100

    IManifest.php, ITransfer.php: formatted

--- src/Bitmovin/api/model/ITransfer.php
+++ src/Bitmovin/api/model/ITransfer.php
@@ -6,11 +6,18 @@ namespace Bitmovin\api\model;
 interface ITransfer
 {
     public function getName();
+
     public function setName($name);
+
     public function getDescription();
+
     public function setDescription($description);
+
     public function getOutputs();
+
     public function setOutputs($outputs);
+
     public function getCloudRegion();
+
     public function setCloudRegion($cloudRegion);
 }
--- src/Bitmovin/api/model/manifests/IManifest.php
+++ src/Bitmovin/api/model/manifests/IManifest.php
@@ -7,7 +7,10 @@ use Bitmovin\api\model\Transferable;
 interface IManifest extends Transferable
 {
     public function getName();
+
     public function setName($name);
+
     public function getOutputs();
+
     public function setOutputs($outputs);
 }
