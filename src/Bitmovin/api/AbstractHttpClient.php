82b334b7bf90598d20671bdfaf04b03d118734c2
Gernot Zwantschko <raeumungsdienst@gmx-topmail.de>
2016-12-12 13:48:43 +0100

    bumped version to v1.3.5

--- src/Bitmovin/api/AbstractHttpClient.php
+++ src/Bitmovin/api/AbstractHttpClient.php
@@ -56,7 +56,7 @@ abstract class AbstractHttpClient
             'X-Api-Key' => $apiKey,
             'Content-Type' => 'application/json',
             'X-Api-Client' => 'bitmovin-php',
-            'X-Api-Client-Version' => '1.3.4'
+            'X-Api-Client-Version' => '1.3.5'
         );
         self::$httpClient = new Client($this->getRequiredOptions());
 
