ca37d8d90b5e3ed325108222954538c625f3e58c
dmoser <david.moser@bitmovin.com>
2017-02-21 13:12:37 +0100

    Adjusted CreateSimpleEncodingWithScalityInput.php to use Scality Output as well

--- examples/CreateSimpleEncodingWithScalityInput.php
+++ examples/CreateSimpleEncodingWithScalityInput.php
@@ -9,7 +9,7 @@ use Bitmovin\configs\manifest\DashOutputFormat;
 use Bitmovin\configs\manifest\HlsOutputFormat;
 use Bitmovin\configs\video\H264VideoStreamConfig;
 use Bitmovin\input\GenericS3Input;
-use Bitmovin\output\GcsOutput;
+use Bitmovin\output\GenericS3Output;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
@@ -19,19 +19,21 @@ $client = new BitmovinClient('INSERT YOUR API KEY HERE');
 $videoInputPath = 'http://eu-storage.bitcodin.com/inputs/Sintel.2010.720p.mkv';
 
 //INPUT CONFIGURATION
-$scalityHost = 's3.yourdomain.com'; // This can also be an ip address of your host where the scality server is running
-$scalityPort = 50123; // The port on which your scality s3 server is listening
-$scalityAccessKey = 'YOURSCALITYS3ACCESSKEY'; // This is the access key that is configured either via environment variables or in the scality configuration file. Refer to this guide https://github.com/scality/S3/blob/master/DOCKER.md#access_key-and-secret_key
-$scalitySecretKey = 'YOURSCALITYS3SECRETKEY'; // This is the secret key configured
-$scalityBucketName = 'YOURSCALITYS3BUCKETNAME'; // The name of the bucket. Make sure you have created this bucket before
-$scalityPrefix = "path/to/your/destination/"; // This is the path where your files will be transferred to. Subdirectories will be generated automatically
-
+$scalityInputHost = 's3.yourdomain.com'; // This can also be an ip address of your host where the scality server is running
+$scalityInputPort = 50123; // The port on which your scality s3 server is listening
+$scalityInputAccessKey = 'YOURSCALITYS3ACCESSKEY'; // This is the access key that is configured either via environment variables or in the scality configuration file. Refer to this guide https://github.com/scality/S3/blob/master/DOCKER.md#access_key-and-secret_key
+$scalityInputSecretKey = 'YOURSCALITYS3SECRETKEY'; // This is the secret key configured
+$scalityInputBucketName = 'YOURSCALITYS3BUCKETNAME'; // The name of the bucket. Make sure you have created this bucket before
+$scalityInputPath = "path/to/your/input/file.mkv"; // This is the path where your files will be transferred to. Subdirectories will be generated automatically
 
 //OUTPUT CONFIGURATION
-$gcsAccessKey = "YOUR GCS OUTPUT ACCESS KEY";
-$gcsSecretKey = "YOUT GCS OUTPUT SECRET KEY";
-$gcsBucketName = "YOUR GCS OUTPUT BUCKET NAME";
-$gcsPrefix = "YOUR GCS OUTPUT PREFIX";
+$scalityOutputHost = 's3.yourdomain.com'; // This can also be an ip address of your host where the scality server is running
+$scalityOutputPort = 50123; // The port on which your scality s3 server is listening
+$scalityOutputAccessKey = 'YOURSCALITYS3ACCESSKEY'; // This is the access key that is configured either via environment variables or in the scality configuration file. Refer to this guide https://github.com/scality/S3/blob/master/DOCKER.md#access_key-and-secret_key
+$scalityOutputSecretKey = 'YOURSCALITYS3SECRETKEY'; // This is the secret key configured
+$scalityOutputBucketName = 'YOURSCALITYS3BUCKETNAME'; // The name of the bucket. Make sure you have created this bucket before
+$scalityOutputPrefix = "path/to/your/destination/"; // This is the path where your files will be transferred to. Subdirectories will be generated automatically
+
 
 // CREATE ENCODING PROFILE
 $encodingProfile = new EncodingProfileConfig();
@@ -40,7 +42,7 @@ $encodingProfile->cloudRegion = CloudRegion::GOOGLE_EUROPE_WEST_1;
 
 // CREATE VIDEO STREAM CONFIG FOR 1080p
 $videoStreamConfig_1080 = new H264VideoStreamConfig();
-$videoStreamConfig_1080->input = new GenericS3Input($scalityBucketName, $scalityAccessKey, $scalitySecretKey, $scalityHost, $scalityPort, $scalityPath);
+$videoStreamConfig_1080->input = new GenericS3Input($scalityInputBucketName, $scalityInputAccessKey, $scalityInputSecretKey, $scalityInputHost, $scalityInputPort, $scalityInputPath);
 $videoStreamConfig_1080->width = 1920;
 $videoStreamConfig_1080->height = 816;
 $videoStreamConfig_1080->bitrate = 4800000;
@@ -49,7 +51,7 @@ $encodingProfile->videoStreamConfigs[] = $videoStreamConfig_1080;
 
 // CREATE VIDEO STREAM CONFIG FOR 720p
 $videoStreamConfig_720 = new H264VideoStreamConfig();
-$videoStreamConfig_720->input = new GenericS3Input($scalityBucketName, $scalityAccessKey, $scalitySecretKey, $scalityHost, $scalityPort, $scalityPath);
+$videoStreamConfig_720->input = new GenericS3Input($scalityInputBucketName, $scalityInputAccessKey, $scalityInputSecretKey, $scalityInputHost, $scalityInputPort, $scalityInputPath);
 $videoStreamConfig_720->width = 1280;
 $videoStreamConfig_720->height = 544;
 $videoStreamConfig_720->bitrate = 2400000;
@@ -58,7 +60,7 @@ $encodingProfile->videoStreamConfigs[] = $videoStreamConfig_720;
 
 // CREATE AUDIO STREAM CONFIG
 $audioConfig = new AudioStreamConfig();
-$audioConfig->input = new GenericS3Input($scalityBucketName, $scalityAccessKey, $scalitySecretKey, $scalityHost, $scalityPort, $scalityPath);
+$audioConfig->input = new GenericS3Input($scalityInputBucketName, $scalityInputAccessKey, $scalityInputSecretKey, $scalityInputHost, $scalityInputPort, $scalityInputPath);
 $audioConfig->bitrate = 128000;
 $audioConfig->rate = 48000;
 $audioConfig->name = 'English';
@@ -70,7 +72,7 @@ $encodingProfile->audioStreamConfigs[] = $audioConfig;
 $jobConfig = new JobConfig();
 
 // ASSIGN GCS OUTPUT
-$jobConfig->output = new GcsOutput($gcsAccessKey, $gcsSecretKey, $gcsBucketName);
+$jobConfig->output = new GenericS3Output($scalityOutputAccessKey, $scalityOutputSecretKey, $scalityOutputHost, $scalityOutputPort, $scalityOutputBucketName, $scalityOutputPrefix);
 $jobConfig->output->prefix = $gcsPrefix;
 // ASSIGN ENCODING PROFILES TO JOB
 $jobConfig->encodingProfile = $encodingProfile;
