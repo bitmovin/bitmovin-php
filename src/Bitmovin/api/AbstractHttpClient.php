f396b0d0f6c2c9832dfa6fb78f87f3e395d6b7d4
Roland Kersche <roland.kersche@bitmovin.com>
2017-08-24 17:21:07 +0200

    Updated to 1.5.9

--- src/Bitmovin/api/AbstractHttpClient.php
+++ src/Bitmovin/api/AbstractHttpClient.php
@@ -56,7 +56,7 @@ abstract class AbstractHttpClient
             'X-Api-Key' => $apiKey,
             'Content-Type' => 'application/json',
             'X-Api-Client' => 'bitmovin-php',
-            'X-Api-Client-Version' => '1.5.8'
+            'X-Api-Client-Version' => '1.5.9'
         );
         self::$httpClient = new Client($this->getRequiredOptions());
 
