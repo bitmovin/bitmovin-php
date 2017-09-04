fdf7ef5e9ca8803139de8f05a8dcd29dc492f2cb
Roland Kersche <roland.kersche@bitmovin.com>
2017-09-04 11:59:30 +0200

    Added HlsVersion enum

---
+++ src/Bitmovin/api/enum/manifests/hls/HlsVersion.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bitmovin\api\enum\manifests\hls;
+
+use Bitmovin\api\enum\AbstractEnum;
+
+class HlsVersion extends AbstractEnum
+{
+    const HLS_VERSION_3 = 3;
+    const HLS_VERSION_4 = 4;
+    const HLS_VERSION_5 = 5;
+    const HLS_VERSION_6 = 6;
+    const HLS_VERSION_7 = 7;
+}
