856476fe25b8bb3724054e8a49586c81564a89e1
Germano Fronza <germano.inf@gmail.com>
2019-02-04 07:28:32 -0200

    Added SINGLE_PASS to encoding mode

--- src/Bitmovin/api/model/encodings/EncodingMode.php
+++ src/Bitmovin/api/model/encodings/EncodingMode.php
@@ -8,6 +8,7 @@ use Bitmovin\api\enum\AbstractEnum;
 class EncodingMode extends AbstractEnum
 {
     const STANDARD = 'STANDARD';
+    const SINGLE_PASS = 'SINGLE_PASS';
     const TWO_PASS = 'TWO_PASS';
     const THREE_PASS = 'THREE_PASS';
 }
